37ca9626f9b153f73071ed12082e194670bffcd6
fadeomar <fadeomar2015@gmail.com>
2020-02-11 18:47:01 +0200

    create team page

---
+++ src/pages/team.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const TeamPage = ({ data }) => (
+  <Layout>
+    <SEO title="Team" />
+    <h1>Team Page</h1>
+  </Layout>
+)
+
+export default TeamPage

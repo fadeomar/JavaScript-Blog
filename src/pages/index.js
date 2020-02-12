90aaf88f2f5e4eb5e099fef450896abac5fc6052
fadeomar <fadeomar2015@gmail.com>
2020-02-12 18:38:39 +0200

    add the fontawesom link and sidbar component to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,31 +4,55 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql, StaticQuery } from "gatsby"
 import Post from "../components/post"
+import { Row, Col } from "reactstrap"
+import Sidebar from "../components/Sidebar"
 
 const IndexPage = ({ data }) => (
   <Layout>
+    <script
+      src="https://kit.fontawesome.com/42295b36e0.js"
+      crossorigin="anonymous"
+    ></script>
     <SEO title="Home" />
     <h1>Home Page</h1>
-    <StaticQuery
-      query={indexQuery}
-      render={data => {
-        return (
-          <div>
-            {data.allMarkdownRemark.edges.map(({ node }) => {
-              return (
-                <Post
-                  title={node.frontmatter.title}
-                  author={node.frontmatter.author}
-                  path={node.frontmatter.path}
-                  date={node.frontmatter.date}
-                  body={node.excerpt}
-                />
-              )
-            })}
-          </div>
-        )
-      }}
-    />
+    <Row>
+      <Col md="8">
+        <StaticQuery
+          query={indexQuery}
+          render={data => {
+            return (
+              <div>
+                {data.allMarkdownRemark.edges.map(({ node }) => {
+                  return (
+                    <Post
+                      key={node.id}
+                      title={node.frontmatter.title}
+                      author={node.frontmatter.author}
+                      slug={node.fields.slug}
+                      date={node.frontmatter.date}
+                      body={node.excerpt}
+                      fluid={node.frontmatter.image.childImageSharp.fluid}
+                      tags={node.frontmatter.tags}
+                    />
+                  )
+                })}
+              </div>
+            )
+          }}
+        />
+      </Col>
+      <Col md="4">
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0,0,0,0.4)",
+          }}
+        >
+          <Sidebar />
+        </div>
+      </Col>
+    </Row>
   </Layout>
 )
 
@@ -43,8 +67,29 @@ const indexQuery = graphql`
             date(formatString: "MMM Do YYYY")
             author
             path
+            tags
+            image {
+              childImageSharp {
+                fluid {
+                  base64
+                  tracedSVG
+                  aspectRatio
+                  src
+                  srcSet
+                  srcWebp
+                  srcSetWebp
+                  sizes
+                  originalImg
+                  originalName
+                  presentationWidth
+                }
+              }
+            }
           }
           excerpt
+          fields {
+            slug
+          }
         }
       }
     }

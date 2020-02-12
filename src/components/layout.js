337f831412cc2ccc5e8997bc12ab469cad937dc2
fadeomar <fadeomar2015@gmail.com>
2020-02-12 20:07:58 +0200

    add slider to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,11 +9,13 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import "../styles/index.scss"
+import { Row, Col } from "reactstrap"
 
 import Header from "./header"
 import Footer from "./Footer"
+import Sidebar from "./Sidebar"
 
-const Layout = ({ children }) => {
+const Layout = ({ children, pageTitle }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -27,12 +29,20 @@ const Layout = ({ children }) => {
     <>
       <script
         src="https://kit.fontawesome.com/42295b36e0.js"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></script>
 
       <Header siteTitle={data.site.siteMetadata.title} />
       <div className="container" id="content">
-        <main>{children}</main>
+        <h1>{pageTitle}</h1>
+
+        <Row>
+          <Col md="8">{children}</Col>
+          <Col md="4">
+            <Sidebar />
+          </Col>
+        </Row>
+        <main></main>
         <Footer />
       </div>
     </>

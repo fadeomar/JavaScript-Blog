5ad1e1021914c94a8bde5b3f7af4c4464336cc94
fadeomar <fadeomar2015@gmail.com>
2020-02-12 18:36:40 +0200

    create the post component

--- src/components/post.js
+++ src/components/post.js
@@ -1,21 +1,44 @@
 import React from "react"
 import { Link } from "gatsby"
+import Img from "gatsby-image"
+import { slugify } from "../util/utility"
 
-import { Card, CardBody, CardTitle, CardText, CardSubtitle } from "reactstrap"
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardText,
+  CardSubtitle,
+  Badge,
+} from "reactstrap"
 
-const Post = ({ title, author, path, date, body }) => {
+const Post = ({ title, author, slug, date, body, fluid, tags }) => {
   return (
     <Card>
+      <Link to={slug}>
+        <Img className="card-image-top" fluid={fluid} />
+      </Link>
       <CardBody>
         <CardTitle>
-          <Link to={path}>{title}</Link>
+          <Link to={slug}>{title}</Link>
         </CardTitle>
         <CardSubtitle>
           <span className="text-info">{date}</span> by{" "}
           <span className="text-info">{author}</span>
         </CardSubtitle>
         <CardText>{body}</CardText>
-        <Link to={path} className="btn btn-outline-primary float-right">
+        <ul className="post-tags">
+          {tags.map(tag => (
+            <li key={tag}>
+              <Link to={`/tags/${slugify(tag)}`}>
+                <Badge color="primary" className="text-uppercase">
+                  {tag}
+                </Badge>
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <Link to={slug} className="btn btn-outline-primary float-right">
           ...Reaad More
         </Link>
       </CardBody>

af45eec16b9802171c864554c83c4b40fe080c16
fadeomar <fadeomar2015@gmail.com>
2020-02-13 14:58:44 +0200

    use the user data in the sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,11 +11,11 @@ import {
 import { graphql, StaticQuery, Link } from "gatsby"
 import Img from "gatsby-image"
 
-const Sidebar = ({ author, authorFluid }) => (
+const Sidebar = ({ author, authorImageFluid }) => (
   <div>
     {author && (
       <Card>
-        <Img className="card-image-top" fluid={authorFluid} />
+        <Img className="card-image-top" fluid={authorImageFluid} />
         <CardBody>
           <CardTitle className="text-center text-uppercase mb-3">
             {author.name}
